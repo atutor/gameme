fd4c714e58c944cdb8f16d6274089de3aa153b64
Greg Gay <greg@atutorspaces.com>
2016-11-26 11:50:58 -0500

    add links to GameMe documentaiton on wiki.atutor.ca

--- instructor_handbook.php
+++ instructor_handbook.php
@@ -1,4 +1,4 @@
 <?php
 
-echo "This is a handbook page for Gamify";
+echo 'See the <a href="http://wiki.atutor.ca/plugins/viewsource/viewpagesrc.action?pageId=15400986">Instructor Handbook</a>';
 ?>
