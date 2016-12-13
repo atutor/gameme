db8dbf34836a70026526ffef44f931ab309a3851
Greg Gay <greg@atutorspaces.com>
2016-12-13 18:46:38 -0500

    add link to Gamifying ATutor Courses

--- instructor_handbook.php
+++ instructor_handbook.php
@@ -1,4 +1,4 @@
 <?php
 
-echo 'See the <a href="http://wiki.atutor.ca/plugins/viewsource/viewpagesrc.action?pageId=15400986">Instructor Handbook</a>';
+echo 'See the <a href="http://wiki.atutor.ca/plugins/viewsource/viewpagesrc.action?pageId=15400986">GameMe Instructor Documentation</a>, or see <a href="http://wiki.atutor.ca/plugins/viewsource/viewpagesrc.action?pageId=15761423">Gamifying ATutor Courses</a>.' ;
 ?>
