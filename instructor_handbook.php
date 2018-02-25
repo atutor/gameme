63572582269f9e94d751801b6e6d0c034d6525a1
atutorlangs~/.ssh <gregrgay@gmail.com>
2018-02-25 16:46:10 -0500

    update links to gameme documentation to point to atutir.github

--- instructor_handbook.php
+++ instructor_handbook.php
@@ -1,4 +1,4 @@
 <?php
 
-echo 'See the <a href="http://wiki.atutor.ca/plugins/viewsource/viewpagesrc.action?pageId=15400986">GameMe Instructor Documentation</a>, or see <a href="http://wiki.atutor.ca/plugins/viewsource/viewpagesrc.action?pageId=15761423">Gamifying ATutor Courses</a>.' ;
+echo 'See the <a href="https://atutor.github.io/atutor/docs/gameme_instructor.html">GameMe Instructor Documentation</a>, or see <a href="https://atutor.github.io/atutor/docs/gameme_gamification.html">Gamifying ATutor Courses</a>.' ;
 ?>
